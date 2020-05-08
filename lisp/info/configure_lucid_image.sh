0dadb98d404906d7231588fdce819324bada05a1
chengzhengqian <chengzhengqian@gmail.com>
2020-05-08 17:15:06 -0400

    add build info for customized imagemagick

---
+++ lisp/info/configure_lucid_image.sh
@@ -0,0 +1,3 @@
+export PKG_CONFIG_PATH=/home/chengzhengqian/Application/imagemagick/imagemagic-6.9.11-install/lib/pkgconfig
+./configure --prefix=/home/chengzhengqian/Application/emacs/emacs-26.3-lucid-with-image/ --with-x-toolkit=lucid  --with-imagemagick --with-modules
+
