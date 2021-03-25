27cea52ef9236a3718caffce428f2fc437c3f77e
chengzhengqian-game <chengzhengqian@gmail.com>
2021-03-25 18:52:09 -0400

    add imenu

---
+++ elpa/imenu-list-0.9/imenu-list-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/chengzhengqian/.emacs.d/elpa/imenu-list-0.9/imenu-list.el  -*- no-byte-compile: t -*-
+(define-package "imenu-list" "0.9" "Show imenu entries in a separate buffer" '((cl-lib "0.5")) :commit "6cded436010a39592175238e4d02263a7cdb44c4" :authors '(("Bar Magal (2015)")) :maintainer '("Bar Magal (2015)") :url "https://github.com/bmag/imenu-list")
