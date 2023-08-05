d6aff3e6645131c2f2babfe59c38b73184da1736
chengzhengqian-game <chengzhengqian@gmail.com>
2023-08-05 17:20:27 -0400

    update compile information for 29.1

--- lisp/info/configure_lucid_image.sh
+++ lisp/info/configure_lucid_image.sh
@@ -7,3 +7,26 @@ export PKG_CONFIG_PATH=/home/chengzhengqian/Application_local/imagemagic-6.9.11-
 # load env
 
 export LD_LIBRARY_PATH=/home/chengzhengqian/Application_local/imagemagic-6.9.11-install/lib
+
+2023.8
+# we update imagemagic to 7.1 in game
+# in /home/chengzhengqian/Application_local/imagemagick/ImageMagick-7.1.1-15
+./configure --prefix=/home/chengzhengqian/Application_local/imagemagick/imagemagick-7.1-install
+
+# check with rsvg, =no. We need to add corresponding library.
+sudo apt install librsvg2-dev 
+# rerun the ./configure. !! option, I also install libdjvulibre-dev, libwebp-dev
+./configure --prefix=/home/chengzhengqian/Application_local/imagemagick/imagemagick-7.1-install --with-rsvg
+# I also add --with-webp
+./configure --prefix=/home/chengzhengqian/Application_local/imagemagick/imagemagick-7.1-install --with-rsvg --with-djvu
+# we can add more, but this should be enough.
+# Then run
+make
+
+# now, we install emacs,
+# see "/home/chengzhengqian/.emacs.d/lisp/info/build-lucid"
+wget http://mirrors.tripadvisor.com/gnu/emacs/emacs-29.1.tar.gz
+# in /home/chengzhengqian/Application_local/emacs/emacs-29.1
+export PKG_CONFIG_PATH=/home/chengzhengqian/Application_local/imagemagick/imagemagick-7.1-install/lib/pkgconfig
+#  it is libjansson-dev after some search
+./configure --prefix=/home/chengzhengqian/Application/emacs/emacs-29-lucid-image-json-module  --with-x-toolkit=lucid  --with-imagemagick --with-modules --with-json   CFLAGS='-O3'
